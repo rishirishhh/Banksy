069a27bb39c07365aad28bbea7e71360e11ea0e0
rishi-19-04 <rishikavya19@gmail.com>
2024-04-28 03:51:49 +0530

    implemented transfer money api

---
+++ api/validator.go
@@ -0,0 +1,16 @@
+package api
+
+import (
+	"Banksy/util"
+
+	"github.com/go-playground/validator/v10"
+)
+
+var validCurrency validator.Func = func(fieldLevel validator.FieldLevel) bool {
+
+	if currency, ok := fieldLevel.Field().Interface().(string); ok {
+		return util.IsSupportedCurrency(currency)
+	}
+
+	return false
+}
